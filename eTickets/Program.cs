c932b7787d6d25e9d111afc2de92732f0ed7b1b1
Mille Elfver <mille.elfver98@gmail.com>
2024-04-19 12:47:53 +0200

    Added connectionstring

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,9 +1,12 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options
+    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
