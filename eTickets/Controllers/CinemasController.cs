ad8debb79a511330e60616c14333f20274ef0bde
Mille Elfver <mille.elfver98@gmail.com>
2024-04-19 14:00:28 +0200

    Added new migration and cinema view

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -14,7 +14,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allCinemas = await _context.Cinemas.ToListAsync();
-            return View();
+            return View(allCinemas);
         }
     }
 }
