7944521a93533ee227e190ece2f8a17c16369d03
Mille Elfver <mille.elfver98@gmail.com>
2024-04-19 17:57:44 +0200

    added actorservice interface

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Create(Actor actor);
+        Actor Update(int id, Actor actor);
+        void Delete(int id);
+    }
+}
