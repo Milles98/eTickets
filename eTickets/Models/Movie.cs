0b65c9648fdcc776755fa108b1b6f3b8eef9d1ed
Mille Elfver <mille.elfver98@gmail.com>
2024-04-20 09:29:46 +0200

    Added movie CRUD

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,9 +1,10 @@
-﻿using eTickets.Data.Enums;
+﻿using eTickets.Data.Base;
+using eTickets.Data.Enums;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
-    public class Movie
+    public class Movie : IEntityBase
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
+builder.Services.AddScoped<IMoviesService, MoviesService>();
 
 builder.Services.AddControllersWithViews();
 
