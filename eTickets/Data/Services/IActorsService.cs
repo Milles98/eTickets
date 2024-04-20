fd6b5bb2c0de0ceec019f96d041999391b80a4d2
Mille Elfver <mille.elfver98@gmail.com>
2024-04-20 07:40:01 +0200

    Added edit view and methods for actor

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -7,7 +7,7 @@ namespace eTickets.Data.Services
         Task<IEnumerable<Actor>> GetAllAsync();
         Task<Actor> GetByIdAsync(int id);
         Task CreateAsync(Actor actor);
-        Actor UpdateAsync(int id, Actor actor);
+        Task<Actor> UpdateAsync(int id, Actor actor);
         void DeleteAsync(int id);
     }
 }
