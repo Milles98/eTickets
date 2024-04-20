476f21a7c7fc0c27f65cc82713d7b6cb0d8cca50
Mille Elfver <mille.elfver98@gmail.com>
2024-04-20 08:34:47 +0200

    added producersservice

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -8,8 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
