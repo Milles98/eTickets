a40b87b36a22809e6c5960b65c009bab8617cfb3
Mille Elfver <mille.elfver98@gmail.com>
2024-05-01 07:40:07 +0200

    Connected my Ads API to etickets, new nav item where you can currently see all ads from the api

---
+++ Library/Models/Advertisement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int IsDeleted { get; set; }
+    }
+
+}
