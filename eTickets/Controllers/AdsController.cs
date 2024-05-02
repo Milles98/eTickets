9542c5718626dfc87ecfe9d1bfc29b63947ba246
Mille Elfver <mille.elfver98@gmail.com>
2024-05-02 06:30:36 +0200

    added login method from api

--- eTickets/Controllers/AdsController.cs
+++ eTickets/Controllers/AdsController.cs
@@ -1,6 +1,9 @@
-﻿using Library.Models;
+﻿using eTickets.Models;
+using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace eTickets.Controllers
@@ -8,10 +11,23 @@ namespace eTickets.Controllers
     public class AdsController : Controller
     {
         private readonly HttpClient _httpClient;
+        private string _jwtToken;
 
         public AdsController(IHttpClientFactory httpClientFactory)
         {
             _httpClient = httpClientFactory.CreateClient(nameof(AdsController));
+        }
+
+        private async Task Login()
+        {
+            var loginModel = new LoginModel { Username = "AdsAdmin", Password = "AdsAdminPassword123!" };
+            var loginJson = JsonSerializer.Serialize(loginModel);
+            var loginData = new StringContent(loginJson, Encoding.UTF8, "application/json");
+            var loginResponse = await _httpClient.PostAsync("https://advertisementapi.azurewebsites.net/Login", loginData);
+            var loginResultJson = await loginResponse.Content.ReadAsStringAsync();
+            var loginResult = JsonSerializer.Deserialize<LoginResult>(loginResultJson);
+            _jwtToken = loginResult.Token;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _jwtToken);
         }
 
         public async Task<IActionResult> Index()
@@ -42,6 +58,21 @@ namespace eTickets.Controllers
             }
 
             return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Advertisement ad)
+        {
+            var json = JsonSerializer.Serialize(ad);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"/Ads/{ad.Id}", data);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ad);
         }
     }
 }
---
+++ eTickets/Controllers/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace eTickets.Controllers
+{
+    internal class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
